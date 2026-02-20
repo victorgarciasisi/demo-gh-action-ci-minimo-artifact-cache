11c132a054192886db22fd382e5c7138720a8b07
victorgarciasisi <45923592+victorgarciasisi@users.noreply.github.com>
2026-02-20 13:07:30 +0100

    Update test_app.py: Force fail

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,8 +5,10 @@ from app import add
 
 class TestAdd(unittest.TestCase):
     def test_add(self) -> None:
-        self.assertEqual(add(2, 3), 5)
-
+        #self.assertEqual(add(2, 3), 5)
+        # forzamos fallo en el test
+        self.assertEqual(add(2, 3), 6)
 
 if __name__ == "__main__":
     unittest.main()
+
