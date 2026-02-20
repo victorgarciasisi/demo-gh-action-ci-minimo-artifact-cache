ff63c5b17fcb3ec040e7d44820360c19baac755e
victorgarciasisi <45923592+victorgarciasisi@users.noreply.github.com>
2026-02-20 13:17:39 +0100

    Update test_app.py: Disallow comments

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,9 +5,10 @@ from app import add
 
 class TestAdd(unittest.TestCase):
     def test_add(self) -> None:
-        #self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 3), 5)
 
 if __name__ == "__main__":
     unittest.main()
+
 
 
